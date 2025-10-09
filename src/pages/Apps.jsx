4263bf252645efbaf498180ed34884fc0a40baeb
rakibhassannayem <rakibhassannayem@gmail.com>
2025-10-09 16:56:20 +0600

    delete timeout

--- src/pages/Apps.jsx
+++ src/pages/Apps.jsx
@@ -1,27 +1,15 @@
 import useApps from "../hooks/useApps";
 import AppCard from "../components/AppCard";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router";
 import LoadingAnimation from "../components/LoadingAnimation";
 
 const Apps = () => {
   const { apps, loading } = useApps();
   const [search, setSearch] = useState("");
-  const [searchLoading, setSearchLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
-
-  useEffect(() => {
-    setSearchLoading(true);
-
-    const timer = setTimeout(() => {
-      setSearchTerm(search.trim("").toLowerCase());
-      setSearchLoading(false);
-    }, 300);
-    return () => clearTimeout(timer);
-  }, [search]);
-
-  const searchedApps = searchTerm
-    ? apps.filter((app) => app.title.toLocaleLowerCase().includes(searchTerm))
+  const term = search.trim("").toLocaleLowerCase();
+  const searchedApps = term
+    ? apps.filter((app) => app.title.toLocaleLowerCase().includes(term))
     : apps;
 
   return (
@@ -47,7 +35,7 @@ const Apps = () => {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-3">
-        {loading || searchLoading ? (
+        {loading ? (
           <div className="col-span-full">
             <LoadingAnimation />
           </div>
